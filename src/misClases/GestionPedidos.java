9c97522e3a158fa087c090b8891af31f67442aec
Leonardo Manrique <leonardomanrique43@gmail.com>
2018-06-22 18:32:33 -0500

    gestion

--- src/misClases/GestionPedidos.java
+++ src/misClases/GestionPedidos.java
@@ -1,15 +1,23 @@
 package misClases;
 
-public class GestionPedidos {
-    private ArrayCola<Pedidos> cola;
+public class GestionPedidos <E extends Pedidos> {
+    private ArrayCola<E> cola;
     
     public GestionPedidos() {
         cola = new ArrayCola<>();
     }
     
-    public ArrayCola<Pedidos> getCola() {
+    public ArrayCola<E> getCola() {
         return cola;
     }
     
+    public void encolarPedido(E x){
+        cola.encolar(x);
+        //falta validar que el num de sucursal no se repita
+    }
+    
+    public void atenderSucursalesPares(){
+        
+    }
     
 }
